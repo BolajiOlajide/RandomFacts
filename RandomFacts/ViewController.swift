352c0e118ff39807534b5da46bd18aaa39451234
Bolaji Olajide <cooproton@gmail.com>
2019-10-03 02:59:50 +0100

    Initial Commit

---
+++ RandomFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomFacts
+//
+//  Created by Bolaji Olajide on 03/10/2019.
+//  Copyright © 2019 Bolaji Olajide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
